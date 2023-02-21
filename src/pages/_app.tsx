9be413b031b0925db06ce36ef1d14af0296fa8f5
dungvv <dungvv@fabbi.com.vn>
2023-02-21 15:29:15 +0700

    feat: add material

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/main.scss'
 
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
 
@@ -12,7 +14,11 @@ type AppPropsWithLayout = AppProps & {
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Component {...pageProps} />
+    </LocalizationProvider>
+  )
 }
 
 export default MyApp
