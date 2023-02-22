16e268e41b945b99a8bb884d08d431b8740a83fd
dungvv <dungvv@fabbi.com.vn>
2023-02-22 13:41:58 +0700

    fix: eslint

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,39 +1,4 @@
-import {
-  CAN,
-  CHI,
-  TIETKHI,
-  TK19,
-  TK20,
-  TK21,
-  TK22,
-  VALIDATE_EMAIL,
-  VALIDATE_REQUIRED,
-} from './constant'
-
-export const validateEmail = (value: any) => {
-  let error = false
-  const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  if (value && !regexEmail.test(value)) {
-    error = true
-  }
-  return error
-}
-const validateRequired = (value: any) => {
-  let error = false
-  if (
-    (typeof value === 'string' && !value.trim().length) ||
-    typeof value === 'undefined' ||
-    value === null
-  ) {
-    error = true
-  }
-  return error
-}
-
-export const handleValidates: { [index: string]: (value: any) => any } = {
-  [VALIDATE_EMAIL]: validateEmail,
-  [VALIDATE_REQUIRED]: validateRequired,
-}
+import { CAN, CHI, TIETKHI, TK19, TK20, TK21, TK22 } from './constant'
 
 export function removeZero(value: string) {
   return +value
