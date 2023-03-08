53bab88f3fb6da3b714bebbcc491f665a687c2c0
dungvv <dungvv@fabbi.com.vn>
2023-03-08 11:36:25 +0700

    fix: tu dai

--- src/api/home.api.ts
+++ src/api/home.api.ts
@@ -3,8 +3,13 @@ import type { ResultResponse } from '@/models'
 import axiosClient from './axios-client'
 
 const homeApi = {
-  async getInfo(month: number, lunarDay: string, lunarDate: string) {
-    const url = `/api/home?month=${month}&lunar_day=${lunarDay}&lunar_date=${lunarDate}`
+  async getInfo(
+    month: number,
+    lunarDay: string,
+    lunarDate: string,
+    tietkhi: string
+  ) {
+    const url = `/api/home?month=${month}&lunar_day=${lunarDay}&lunar_date=${lunarDate}&tiet_khi=${tietkhi}`
     const res = await axiosClient.get<ResultResponse<any>>(url)
     return res.data
   },
